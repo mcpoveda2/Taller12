e2972dbe6fb8369d869afc9c3b1f6910fbaebec5
mcpoveda2 <mcpoveda@espol.edu.ec>
2024-08-19 16:24:29 -0500

    Test Sumas

--- TallerTest/src/test/java/com/mycompany/tallertest/CalculadoraTest.java
+++ TallerTest/src/test/java/com/mycompany/tallertest/CalculadoraTest.java
@@ -36,13 +36,65 @@ public class CalculadoraTest {
     public void tearDown() {
     }
 
+    
     @Test
-    public void testSuma() {
+    public void testSuma_TCS01() {
+        System.out.println("suma");
+        double a = -3.0;
+        double b = -5.0;
+        Calculadora instance = new Calculadora();
+        double expResult = -8.0;
+        double result = instance.suma(a, b);
+        assertEquals(expResult, result, 0);
+        //fail("The test case is a prototype.");
+    }
+    
+    
+    @Test
+    public void testSuma_TCS02() {
         System.out.println("suma");
         double a = 0.0;
         double b = 0.0;
         Calculadora instance = new Calculadora();
         double expResult = 0.0;
+        double result = instance.suma(a, b);
+        assertEquals(expResult, result, 0);
+        //fail("The test case is a prototype.");
+    }
+    
+    
+    
+    @Test
+    public void testSuma_TCS03() {
+        System.out.println("suma");
+        double a = 10.0;
+        double b = 5.0;
+        Calculadora instance = new Calculadora();
+        double expResult = 15.0;
+        double result = instance.suma(a, b);
+        assertEquals(expResult, result, 0);
+        //fail("The test case is a prototype.");
+    }
+    
+    @Test
+    public void testSuma_TCS04() {
+        System.out.println("suma");
+        double a = -7.0;
+        double b = 3.0;
+        Calculadora instance = new Calculadora();
+        double expResult = -4.0;
+        double result = instance.suma(a, b);
+        assertEquals(expResult, result, 0);
+        //fail("The test case is a prototype.");
+    }
+    
+    @Test
+    public void testSuma_TCS05() {
+        System.out.println("suma");
+        double a = 7.0;
+        double b = -3.0;
+        Calculadora instance = new Calculadora();
+        double expResult = 4.0;
         double result = instance.suma(a, b);
         assertEquals(expResult, result, 0);
         //fail("The test case is a prototype.");
